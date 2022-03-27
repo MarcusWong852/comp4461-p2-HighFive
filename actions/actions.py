63000db4c0bcbdbdc06ba0c08eb9cedd5203dc87
pzb-85 <oscar2002224@live.com>
2022-03-27 18:50:17 +0800

    good morning/evening
    Added the ability to say good morning/evening according to time of day

--- actions/actions.py
+++ actions/actions.py
@@ -1,3 +1,4 @@
+import datetime
 from typing import Any, Text, Dict, List
 from api.api import API
 from rasa_sdk import Action, Tracker
@@ -18,3 +19,20 @@ class ActionWeatherForecast(Action):
         dispatcher.utter_message(
             text=f"The temperature now is {temperature}C, {weatherForecast}")
         return []
+
+class GreetTimeofDay(Action):
+
+    def name(self) -> Text:
+        return "action_greet_timeofday"
+
+    def run(self, dispatcher: CollectingDispatcher,
+                tracker: Tracker,
+                domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+            
+            dt=datetime.datetime.now()
+            if dt.hour < 12:
+                dispatcher.utter_message(text=f"Good Morning!")
+            else:
+                dispatcher.utter_message(text=f"Good Evening!")
+
+            return []
